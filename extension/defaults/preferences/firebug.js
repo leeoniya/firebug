bdab1a8e1797bfcc38ce25d96667d2849ea5b11c
Leon Sorokin <leeoniya@gmail.com>
2012-06-07 18:58:38 -0500

    nit

--- extension/defaults/preferences/firebug.js
+++ extension/defaults/preferences/firebug.js
@@ -102,9 +102,9 @@ pref("extensions.firebug.showDOMProps", true);
 pref("extensions.firebug.showDOMFuncs", false);
 pref("extensions.firebug.showDOMConstants", false);
 pref("extensions.firebug.showInlineEventHandlers", false);
-pref("extensions.firebug.ObjectShortIteratorMax", 3);
-pref("extensions.firebug.showEnumerableProperties", true);
 pref("extensions.firebug.showOwnProperties", false);
+pref("extensions.firebug.showEnumerableProperties", true);
+pref("extensions.firebug.ObjectShortIteratorMax", 3);
 
 // Layout
 pref("extensions.firebug.showRulers", true);
